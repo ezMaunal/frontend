eb829aede800bad0283e10e8416425787df6f2a2
channykim00 <hadeskim0429@gmail.com>
2025-06-09 19:49:59 +0900

    feat: Repository UI 구현 및 pages 폴더 구조 개편 (#7)

--- src/sidepanel/App.jsx
+++ src/sidepanel/App.jsx
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
 import MainPage from "@/sidepanel/pages/MainPage";
+import Repository from "@/sidepanel/pages/Repository";
 
 const App = () => {
-  return <MainPage />;
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<MainPage />}
+      />
+      <Route
+        path="/repository"
+        element={<Repository />}
+      />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/sidepanel/components/BackButton.jsx
@@ -0,0 +1,11 @@
+import "@/styles/styles.css";
+
+const BackButton = () => {
+  return (
+    <div className="absolute top-0 left-0 mt-2 ml-3">
+      <button className="text-3xl text-black">&larr;</button>
+    </div>
+  );
+};
+
+export default BackButton;
