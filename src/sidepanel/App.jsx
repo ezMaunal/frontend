a181f92e08f975fb0fe8b246b7ff1476bb079d07
hangang1 <152799387+hangang1@users.noreply.github.com>
2025-06-09 23:37:01 +0900

    feat:  Settings UI 페이지 구성 (#6)
    * feat: Settings UI 컴포넌트 생성 (#6)
    
    * feat: Settings UI 컴포넌트 생성 (#6)

--- src/sidepanel/App.jsx
+++ src/sidepanel/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import MainPage from "@/sidepanel/pages/MainPage";
 import Repository from "@/sidepanel/pages/Repository";
+import Settings from "@/sidepanel/pages/Settings";
 import TaskBoard from "@/sidepanel/pages/Taskboard";
 
 const App = () => {
@@ -18,6 +19,10 @@ const App = () => {
       <Route
         path="/taskboard"
         element={<TaskBoard />}
+      />
+      <Route
+        path="/settings"
+        element={<Settings />}
       />
     </Routes>
   );
