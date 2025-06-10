82de7a268ab3e2090023aa013997c2003409bc70
hangang1 <152799387+hangang1@users.noreply.github.com>
2025-06-10 10:18:46 +0900

    feat: Login, Option, DeleteAccount, NoticeDeleteAccount UI 컴포넌트 생성(#12) (#13)

--- src/sidepanel/App.jsx
+++ src/sidepanel/App.jsx
@@ -1,8 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 
+import Login from "@/sidepanel/pages/Login";
 import MainPage from "@/sidepanel/pages/MainPage";
 import Repository from "@/sidepanel/pages/Repository";
 import Settings from "@/sidepanel/pages/Settings";
+import DeleteAccount from "@/sidepanel/pages/Settings/DeleteAccount";
+import NoticeDeleteAccount from "@/sidepanel/pages/Settings/DeleteAccount/NoticeDeleteAccount";
+import Option from "@/sidepanel/pages/Settings/Option";
 import TaskBoard from "@/sidepanel/pages/Taskboard";
 
 const App = () => {
@@ -23,6 +27,22 @@ const App = () => {
       <Route
         path="/settings"
         element={<Settings />}
+      />
+      <Route
+        path="/deleteaccount"
+        element={<DeleteAccount />}
+      />
+      <Route
+        path="/noticedeleteaccount"
+        element={<NoticeDeleteAccount />}
+      />
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+      <Route
+        path="/option"
+        element={<Option />}
       />
     </Routes>
   );
--- src/sidepanel/pages/Settings/index.jsx
+++ src/sidepanel/pages/Settings/index.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 import "@/styles/styles.css";
 import BackButton from "@/sidepanel/components/BackButton";
 
 const Settings = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="main-container">
       <BackButton />
@@ -10,10 +14,16 @@ const Settings = () => {
       </div>
 
       <div className="flex flex-col items-center justify-between space-y-4">
-        <button className="mt-17 w-48 rounded-2xl bg-orange-500 py-5 text-lg font-bold text-white hover:bg-orange-600">
+        <button
+          className="mt-17 w-48 rounded-2xl bg-orange-500 py-5 text-lg font-bold text-white hover:bg-orange-600"
+          onClick={() => navigate("/option")}
+        >
           옵션
         </button>
-        <button className="mt-12 w-48 rounded-2xl bg-orange-500 py-5 text-lg font-bold text-white hover:bg-orange-600">
+        <button
+          className="mt-12 w-48 rounded-2xl bg-orange-500 py-5 text-lg font-bold text-white hover:bg-orange-600"
+          onClick={() => navigate("/deleteaccount")}
+        >
           회원탈퇴
         </button>
       </div>
