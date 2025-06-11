0d42370c13385b6412cdb105fa1b877f3cf5f61b
channykim00 <hadeskim0429@gmail.com>
2025-06-11 21:33:35 +0900

    feat: 요소 클릭 시, 요소 크기만큼 도형 오버레이 표시 구현

--- src/content/captureOverlay.js
+++ src/content/captureOverlay.js
@@ -18,6 +18,18 @@ if (!window.captureOverlayExist) {
     const textContent = target.textContent;
     const rect = target.getBoundingClientRect();
 
+    const overlay = document.createElement("div");
+    overlay.id = "red-outline-box";
+    overlay.style.position = "absolute";
+    overlay.style.top = `${window.scrollY + rect.top - 6}px`;
+    overlay.style.left = `${window.scrollX + rect.left - 6}px`;
+    overlay.style.width = `${rect.width + 12}px`;
+    overlay.style.height = `${rect.height + 12}px`;
+    overlay.style.border = "5px solid rgb(174, 191, 0)";
+    overlay.style.zIndex = "999999";
+    overlay.style.pointerEvents = "none";
+    document.body.appendChild(overlay);
+
     const targetElementInfo = {
       tagName: tagName,
       textContent: textContent,
