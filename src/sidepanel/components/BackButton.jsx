7892cecbbdce515e207508d3d04f6785bc483e88
im <ldh9904@gmail.com>
2025-06-14 16:09:31 +0900

    feat: backbutton-navigate-funtion

--- src/sidepanel/components/BackButton.jsx
+++ src/sidepanel/components/BackButton.jsx
@@ -1,9 +1,21 @@
 import "@/styles/styles.css";
+import { useNavigate } from "react-router-dom";
 
 const BackButton = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="absolute top-0 left-0 mt-2 ml-3">
-      <button className="text-3xl text-black">&larr;</button>
+      <button
+        className="text-3xl text-black"
+        onClick={goBack}
+      >
+        &larr;
+      </button>
     </div>
   );
 };
