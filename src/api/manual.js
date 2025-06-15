8537b5b99302ca5843abed3c0225339e12ec8d69
channykim00 <hadeskim0429@gmail.com>
2025-06-15 19:13:21 +0900

    feat: VITE_BACKEND_SERVER_URL로 백엔드 서버 주소 추가 및 accessToken은 임시로 하드코딩 처리
    - VITE_BACKEND_SERVER_URL 환경 변수를 사용해 서버 URL을 직접 입력하지 않도록 수정
    - 로그인 기능이 아직 구현되지 않아 accessToken은 임시로 하드코딩된 상태

--- src/api/manual.js
+++ src/api/manual.js
@@ -1,9 +1,24 @@
 import axios from "axios";
 
-export const postManual = async (data, token) => {
-  return await axios.post("/api/manual", data, {
+import { dataURLtoFile } from "@/utils/dataURLtoFile";
+
+const postManual = async (data) => {
+  const accessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjMTQ5NmJmMy1mNDZjLTQ3ZTMtOGMwMC0xOTUxYzZiMGIzMzQiLCJrYWthb0lkIjoiNDI5NzE2NDcwMSIsImlhdCI6MTc0OTk3Nzg2OCwiZXhwIjoxNzQ5OTk5NDY4fQ.GawUumgPYIBD2yLPoendI3OEVXKKDQXYTxqla7JZpPA";
+  const serverURL = import.meta.env.VITE_BACKEND_SERVER_URL;
+
+  const formData = new FormData();
+  data.steps.forEach((step, i) => {
+    formData.append("image", dataURLtoFile(step.image, `step-${i}.png`));
+    formData.append("text", step.text);
+  });
+  const response = await axios.post(serverURL + "/api/manual", formData, {
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: `Bearer ${accessToken}`,
     },
   });
+
+  return response.data;
 };
+
+export default postManual;
